009ced9540c61873d8b5098e698797cabc7a3c73
Bradley Bricknell <bradleybricknell@yahoo.com>
2017-04-15 01:01:18 +0100

    #1 initial commit

---
+++ PingOfInterest/app/src/main/java/com/reddigger/PingOfInterest/PintIt/PingIt.java
@@ -0,0 +1,42 @@
+package com.reddigger.PingOfInterest.PintIt;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.MenuItem;
+
+public class PingIt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton pingIt = (FloatingActionButton) findViewById(R.id.pingIt);
+        pingIt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // mongo controller
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
